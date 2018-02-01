0e6213a6fe6b83aaf62ab81901a4eaa2c15a882a
José María Mateos <chema@rinzewind.org>
2018-01-31 19:31:05 -0500

    Proper axis labels

--- plot_data.R
+++ plot_data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 theme_set(theme_bw(12))
 library(lubridate)
+library(scales)
 
 save_username_plot <- function(username, save = TRUE) {
     dd <- read_csv(sprintf("%s.csv.gz", username))
@@ -9,13 +10,16 @@ save_username_plot <- function(username, save = TRUE) {
     dd$created_at <- parse_date_time(dd$created_at,
                                      orders = c("%a %b %d %H:%M:%S %z %Y"))
     dd <- dd[dd$created_at > as.POSIXct("2006-01-01 00:00:00"), ]
+
+    point <- format_format(big.mark = ".", decimal.mark = ",", scientific = FALSE)
     
     plt1 <- ggplot(dd) +
         geom_point(aes(x = order, y = created_at), 
                    color = "blue", alpha = 0.1, size = 0.1) +
         xlab(sprintf("@%s's followers", username)) +
         ylab("Join date") +
-        scale_y_datetime(date_breaks = "1 year", date_labels = "%Y")
+        scale_y_datetime(date_breaks = "1 year", date_labels = "%Y") +
+        scale_x_continuous(labels = point)
     
     if (save == TRUE) {
         ggsave(sprintf("%s.png", username), dpi = 100, width = 9, height = 6)
